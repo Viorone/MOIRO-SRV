252a8feb77994bcc62fd0db80e12ec50f39e3f4c
Андрей Кезик <kezik.1992@gmail.com>
2020-04-08 08:35:37 +0300

    v 0.20

---
+++ MOIRO SRV/Models/AppDbInitializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MOIRO_SRV.Models
+{
+    public class AppDbInitializer : CreateDatabaseIfNotExists<ServerContext>
+    {
+        protected override void Seed(ServerContext context)
+        {
+            ServerContext db = new ServerContext();
+            db.ServerUsers.Add(new ServerUser { Email = "root", Password = "235Cryptocertus" });
+            db.Statuses.Add(new Status { Name = "В обработке" });
+            db.Statuses.Add(new Status { Name = "Выполняется" });
+            db.Statuses.Add(new Status { Name = "Выполнено" });
+            db.Statuses.Add(new Status { Name = "Требуется ремонт/закупка" });
+            db.Statuses.Add(new Status { Name = "Отменено" });
+            db.SaveChanges();
+
+            base.Seed(context);
+        }
+    }
+}
--- MOIRO SRV/Models/ServerContext.cs
+++ MOIRO SRV/Models/ServerContext.cs
@@ -8,12 +8,19 @@ namespace MOIRO_SRV.Models
 {
     public class ServerContext : DbContext
     {
+        static ServerContext()
+        {
+            Database.SetInitializer(new AppDbInitializer());
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Event> Events { get; set; }
         public DbSet<PublicChat> PublicChats { get; set; }
         public DbSet<Status> Statuses { get; set; }
         public DbSet<ServerUser> ServerUsers { get; set; }
+
+       
 
     }
 }
