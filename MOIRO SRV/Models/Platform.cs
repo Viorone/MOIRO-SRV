cb8be5e8351ee433c2165e1b37ef6f0a6ad4e4f5
Viorone <viorone1997@gmail.com>
2020-04-24 16:56:42 +0300

    v 0.28

--- MOIRO SRV/Models/Platform.cs
+++ MOIRO SRV/Models/Platform.cs
@@ -9,6 +9,7 @@ namespace MOIRO_SRV.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int SessionLimit { get; set; }
+        public int SessionLimit { get; set; } //максимальное количество одновременных трансляций
+        public int ListenersCount { get; set; } // максимальное количество слушателей
     }
 }
